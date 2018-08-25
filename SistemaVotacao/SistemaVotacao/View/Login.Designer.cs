5185e51e37bbca3a3e16593377f33661e79d995f
CleitonS <cleiton1302@gmail.com>
2018-08-24 22:02:35 -0300

    Ajustes no layout

--- SistemaVotacao/SistemaVotacao/View/Login.Designer.cs
+++ SistemaVotacao/SistemaVotacao/View/Login.Designer.cs
@@ -34,11 +34,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.bNew = new System.Windows.Forms.Button();
             this.bEnter = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tbPass
             // 
-            this.tbPass.Location = new System.Drawing.Point(111, 138);
+            this.tbPass.Location = new System.Drawing.Point(111, 157);
             this.tbPass.Name = "tbPass";
             this.tbPass.Size = new System.Drawing.Size(144, 20);
             this.tbPass.TabIndex = 11;
@@ -46,7 +48,7 @@
             // 
             // tbLogin
             // 
-            this.tbLogin.Location = new System.Drawing.Point(111, 89);
+            this.tbLogin.Location = new System.Drawing.Point(111, 108);
             this.tbLogin.Name = "tbLogin";
             this.tbLogin.Size = new System.Drawing.Size(144, 20);
             this.tbLogin.TabIndex = 10;
@@ -54,7 +56,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(56, 141);
+            this.label2.Location = new System.Drawing.Point(56, 160);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 9;
@@ -63,7 +65,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(56, 92);
+            this.label1.Location = new System.Drawing.Point(56, 111);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 8;
@@ -71,7 +73,7 @@
             // 
             // bNew
             // 
-            this.bNew.Location = new System.Drawing.Point(136, 249);
+            this.bNew.Location = new System.Drawing.Point(135, 243);
             this.bNew.Name = "bNew";
             this.bNew.Size = new System.Drawing.Size(75, 23);
             this.bNew.TabIndex = 7;
@@ -82,7 +84,7 @@
             // 
             // bEnter
             // 
-            this.bEnter.Location = new System.Drawing.Point(137, 188);
+            this.bEnter.Location = new System.Drawing.Point(136, 208);
             this.bEnter.Name = "bEnter";
             this.bEnter.Size = new System.Drawing.Size(74, 29);
             this.bEnter.TabIndex = 6;
@@ -90,11 +92,34 @@
             this.bEnter.UseVisualStyleBackColor = true;
             this.bEnter.Click += new System.EventHandler(this.bEnter_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(79, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Sistema de Votação ";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(47, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(251, 26);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Um sistema democrático para lhe ajudar a escolher onde ir almoçar com seus amigos" +
+    "";
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(331, 373);
+            this.ClientSize = new System.Drawing.Size(339, 307);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.tbPass);
             this.Controls.Add(this.tbLogin);
             this.Controls.Add(this.label2);
@@ -118,6 +143,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button bNew;
         private System.Windows.Forms.Button bEnter;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- SistemaVotacao/SistemaVotacao/View/MySession.Designer.cs
+++ SistemaVotacao/SistemaVotacao/View/MySession.Designer.cs
@@ -47,9 +47,9 @@
             this.lWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lWelcome.Location = new System.Drawing.Point(12, 9);
             this.lWelcome.Name = "lWelcome";
-            this.lWelcome.Size = new System.Drawing.Size(182, 24);
+            this.lWelcome.Size = new System.Drawing.Size(187, 24);
             this.lWelcome.TabIndex = 0;
-            this.lWelcome.Text = "Bem Vindo <USER>";
+            this.lWelcome.Text = "Bem Vindo, <USER>";
             this.lWelcome.Click += new System.EventHandler(this.lWelcome_Click);
             // 
             // lDay
